1162cb0e27c0f4b690296166ea992867fcd8068e
oreste <muhirwa.g.oreste@gmail.com>
2022-11-14 16:13:04 +0100

    Le main

--- main.cpp
+++ main.cpp
@@ -1,6 +1,36 @@
 #include <iostream>
+#include <unordered_map>
+#include "Plateau.h"
+#include "Position.h"
+#include "Jeu.h"
+
+using namespace std;
+
+
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    Plateau p=Plateau(5);
+    p.afficher();
+
+
+
+    //p.moveDown();p.moveDown();
+    cout<<endl;
+    p.afficher();
+    //p(2,4)=JAGUAR;
+    //cout<<endl<<p;
+    /*Position ps=Position (5);
+    Joueur joueur=Joueur();
+    Jeu jeu=Jeu(joueur,p);
+    jeu.jouer();
+
+   p.fixerCase(ps,joueur);*/
+    //p.afficher();
+    std::cout<<"Hello jam"<<std::endl;
+
+
+
     return 0;
 }
+
