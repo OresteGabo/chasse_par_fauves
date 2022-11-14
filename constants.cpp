0868e95460a7f446284651362413c27b5135ba49
oreste <muhirwa.g.oreste@gmail.com>
2022-11-14 15:21:31 +0100

    Occupants, et Directions sont predefinits en constantes pour faciliter les operations, ainsi que la taille min du plateau

---
+++ constants.cpp
@@ -0,0 +1,25 @@
+//
+// Created by oreste on 09/11/22.
+//
+
+enum Occupants{
+    VIDE=0,
+    LION=1,
+    CHIEN=2,
+    JAGUAR=3,
+    PIEGE=4,
+    OBSTACLE=5,
+    JOUEUR=6
+};
+
+enum Directions{
+    NORD,
+    EST,
+    OUEST,
+    SUD,
+    NORD_OUEST,
+    NORD_EST,
+    SUD_OUEST,
+    SUD_EST
+};
+const unsigned int TAILLE_MIN=6;
